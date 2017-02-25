542fa2a26e2632d997e9174a9daf9f5c62bd1886
Marty Marshalek <mmarshalek@outlook.com>
2017-02-25 12:19:44 -0500

    Initial work on login system

--- net/server_listen.py
+++ net/server_listen.py
@@ -1,1 +1,26 @@
-## Empty file for server listener code
+import MySQLdb
+
+class User:
+    userCount = 0
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.isLoggedIn = false
+
+    def login(self, db):
+        cur = db.cursor()
+        cur.execute()    
+    def register(self, db):
+    
+    def __del__(self):
+        usercount -= 1
+
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", user="uniclick", 
+                        password="bluepolo", db="uniclick")
+    db.close()
+
