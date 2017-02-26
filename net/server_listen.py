443e341c32ea7447a49d9140cb63476f93d1a400
Marty Marshalek <mmarshalek@outlook.com>
2017-02-26 08:40:02 -0500

    More additions to User class

--- net/server_listen.py
+++ net/server_listen.py
@@ -3,16 +3,40 @@ import MySQLdb
 class User:
     userCount = 0
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self):
         self.isLoggedIn = false
+        userCount += 1
 
     def login(self, db):
-        cur = db.cursor()
-        cur.execute()    
+        if self.isLoggedIn == true:
+            print("User is already logged in!!")
+        
+        else:
+            cur = db.cursor()
+            select_stmt = "select username,password from user_auth \
+                           where username=\"{0}\""
+            cur.execute(select_stmt.format(username))
+            
+            for username, password in cur.fetchall():
+                if self.password != password:
+                    print("Password is incorrect!!")
+                elif self.password == password:
+                    print("Logged in as user: " + username)
+                    self.isLoggedIn = true
+                
     def register(self, db):
-    
+        if self.isLoggedIn == true:
+            print("User is already logged in!!")
+        
+        else:
+            self.username = "Enter a username: "
+            self.password = "Enter a password: "
+            cur = db.cursor()
+            insert_stmt = "select username,password from user_auth \
+                           where username=\"{0}\""
+            cur.execute(insert_stmt.format(username,password))
+            
+
     def __del__(self):
         usercount -= 1
 
@@ -21,6 +45,13 @@ class User:
 if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", user="uniclick", 
-                        password="bluepolo", db="uniclick")
+                        passwd="bluepolo", db="uniclick")
+
+    # Main menu 
+    print("Enter a username and password to login")
+    username = input("Enter a Username: ")
+    password = input("Enter a Password: ")
+
+    
     db.close()
 
