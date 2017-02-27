52a465835f497c89dfbaf546f69cdaf0c709ff10
Marty Marshalek <mmarshalek@outlook.com>
2017-02-27 12:30:37 -0500

    server ready-ish

--- net/client.py
+++ net/client.py
@@ -2,7 +2,7 @@ import socket
 
 sock = socket.socket(socket.AF_INET, 
                      socket.SOCK_STREAM)
-host = "45.55.163.153"
+host = socket.gethostname()
 port = 8080
 sock.connect((host, port))
 
--- net/socket_listen.py
+++ net/socket_listen.py
@@ -1,12 +1,43 @@
 import socket
 import sys
-from thread import *
+import threading
 
-host = 'localhost'
-port = 8080
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
 
-try:
-    socket.bind((host, port))
+    def listen(self):
+        self.sock.listen(5)
+        while(1):
+            client, addr = self.sock.accept()
+            client.settimeout(60)
+            thr = threading.Thread(target=self.listenToClient,
+                                   args=(client, addr))
+            thr.daemon = True
+            thr.start()
+
+    def listenToClient(self, client, addr):
+        size = 4096
+        while(1):
+            try:
+                recv_msg = client.recv(size)
+                if recv_msg:
+                    resp_msg = "Received: " + recv_msg
+                    client.send(resp_msg)
+                else:
+                    print("Client disconnected")
+                    raise
+            except:
+                client.close()
+                return False
+
+
+if __name__ == "__main__":
+    port = 8080
+    host = socket.gethostname()
+    Server(host, port).listen()
