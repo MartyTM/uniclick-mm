fa4db29b82ad9ad80f3136cd87fb01e0582f4962
Marty Marshalek <mmarshalek@outlook.com>
2017-02-28 13:24:24 -0500

    some simple tweaks

--- net/tcpserver.py
+++ net/tcpserver.py
@@ -1,17 +1,16 @@
 import socketserver
-import socket
 
 
 class TCP_handler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        print(str(self.data))
         self.request.sendall(self.data.upper())
 
 
 if __name__ == "__main__":
-    host = socket.gethostbyname(socket.gethostname())
+    host = "45.55.163.153"
     port = 8080
 
     server = socketserver.TCPServer((host, port), TCP_handler)
