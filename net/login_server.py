2495d884c4df0688f0db22df36f342c5cdc80fca
Marty Marshalek <mmarshalek@outlook.com>
2017-02-28 21:33:45 -0500

    new playground

---
+++ net/login_server.py
@@ -0,0 +1,87 @@
+import MySQLdb
+import socketserver
+
+"""
+class User:
+    userCount = 0
+
+    # Might be better to init uname and pword to None type
+    def __init__(self, db, uname=None, pword=None):
+        self.isLoggedIn = False
+        self.username = uname
+        self.password = pword
+        self.db = db
+        self.userCount += 1
+
+    def login(self):
+        if self.isLoggedIn is True:
+            print("User is already logged in!!")
+
+        else:
+            self.username = input("Enter a username: ")
+            self.password = input("Enter a password: ")
+            cur = self.db.cursor()
+            select_stmt = "select username,password from user_auth \
+                           where username=\"{0}\""
+            cur.execute(select_stmt.format(self.username))
+
+            for username, password in cur.fetchall():
+                if self.password != password:
+                    print("Password is incorrect!!")
+                elif self.password == password:
+                    print("Logged in as user: " + username)
+                    self.isLoggedIn = True
+
+    def checkLogin(self):
+        if self.isLoggedIn is True:
+            return True
+        else:
+            return False
+
+    def logout(self):
+        self.isLoggedIn = False
+
+    def register(self):
+        cur = self.db.cursor()
+        select_stmt = "select username,password from user_auth \
+                        where username=\"{0}\""
+        self.username = input("Enter a username: ")
+        self.password = input("Enter a password: ")
+        self.id = input("Enter you student ID number: ")
+        if self.isLoggedIn is True:
+            print("User is already logged in!!")
+
+        elif cur.execute(select_stmt.format(self.username)) == 1:
+            print("User already registered")
+
+        else:
+            insert_stmt = "insert into user_auth (username, password, id) \
+                           values(\"{0}\", \"{1}\", {2})"
+
+            cur.execute(insert_stmt.format(
+                        self.username, self.password, self.id))
+            self.db.commit()
+
+    def __del__(self):
+        self.userCount -= 1
+"""
+
+class TCP_handler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.request.sendall("Enter an option: ")
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        data_str = self.data.decode("utf-8/")
+        print(data_str)
+        self.request.sendall(self.data.upper())
+
+
+if __name__ == "__main__":
+    host, port = "45.55.163.153", 8080
+    users = []
+    session = 0
+    db = MySQLdb.connect(host="localhost", user="uniclick",
+                         passwd="bluepolo", db="uniclick")
+    server = socketserver.TCPServer((host, port), TCP_handler)
+    server.serve_forever()
+
