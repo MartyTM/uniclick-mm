6b795f765a639bf61d5769c6d50760d3fa07f007
Marty Marshalek <mmarshalek@outlook.com>
2017-03-11 22:15:52 -0500

    str decoding

--- net/login_server.py
+++ net/login_server.py
@@ -91,12 +91,14 @@ class TCP_handler(socketserver.BaseRequestHandler):
         print(data_str)
         self.request.sendall(self.data.upper())
         if data_str == "LOGIN":
-            username = self.request.recv(1024).strip().decode("utf-8")
+            username = self.request.recv(1024).strip()
             print(username)
             self.request.sendall(username.upper())
-            password = self.request.recv(1024).strip().decode("utf-8")
+            password = self.request.recv(1024).strip()
             print(password)
             self.request.sendall(password.upper())
+            username = username.decode("utf-8")
+            password = password.decode("utf-8")
             TCP_handler.users[self.client_address].login(username, password)
             if TCP_handler.users[self.client_address].isLoggedIn is True:
                 print("Login successful")
